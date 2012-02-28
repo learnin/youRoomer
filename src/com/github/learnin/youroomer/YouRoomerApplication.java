96a5f3cdf58b257040ed3222f4b51d3ec26dd438
learnin <i.learnin@gmail.com>
2012-02-29 02:15:25 +0900

    不要な実装を削除
    APIドキュメントによるとApplication#onTerminateは実機では呼ばれないようなので削除

--- src/com/github/learnin/youroomer/YouRoomerApplication.java
+++ src/com/github/learnin/youroomer/YouRoomerApplication.java
@@ -20,12 +20,6 @@ import android.app.Application;
 public class YouRoomerApplication extends Application {
 
 	@Override
-	public void onTerminate() {
-		super.onTerminate();
-		UserImageCache.getInstance().clear();
-	}
-
-	@Override
 	public void onLowMemory() {
 		super.onLowMemory();
 		UserImageCache.getInstance().clear();
